e8e42c33d553c8bb3a505ab406bd83998e328683
Kyumin Lee <2km4890@gmail.com>
2025-05-01 14:45:04 -0500

    re initializing app with ts and tailwind

--- frontend/types/ethereum.d.ts
+++
@@ -1,9 +0,0 @@
-interface Window {
-    ethereum?: {
-      isMetaMask?: boolean
-      request: (request: { method: string; params?: Array<any> }) => Promise<any>
-      on: (eventName: string, callback: (...args: any[]) => void) => void
-      removeListener: (eventName: string, callback: (...args: any[]) => void) => void
-    }
-  }
-  
